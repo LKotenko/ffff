f03bad2dce512b139a14383251191498e1f8b282
LKotenko <104939055+LKotenko@users.noreply.github.com>
2022-06-30 13:13:18 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+'use script';
+
+
+$(document).ready(function() {
+	$('.slider').slick({
+		arrows: false,
+		dots: true,
+		speed: 2000,
+		autoplay: true,
+		autoplaySpeed: 1500,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+	});
+});
